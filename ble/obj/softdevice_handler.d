6e4f6dea4ebf18e9a49f419949f304cafcd1ae53
pearl2015 <huangpan2015@outlook.com>
2016-06-24 13:57:55 +0900

    version1
    ble_nrf51822

---
+++ ble/obj/softdevice_handler.d
@@ -0,0 +1,35 @@
+obj/softdevice_handler.o: sdk/sd_common/softdevice_handler.c \
+ include/sd_common/softdevice_handler.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stddef.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/reent.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/_default_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/lock.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/stdlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/alloca.h \
+ include/nordic_common.h include/s110/nrf_sdm.h include/s110/nrf_svc.h \
+ include/nrf51.h include/gcc/core_cm0.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stdint.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdint.h \
+ include/gcc/core_cmInstr.h include/gcc/core_cmFunc.h \
+ include/system_nrf51.h include/s110/nrf_soc.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stdbool.h \
+ include/nrf51_bitfields.h include/s110/nrf_error_soc.h \
+ include/s110/nrf_error.h include/s110/nrf_error_sdm.h \
+ include/app_common/app_error.h include/s110/nrf_error.h \
+ include/app_common/app_scheduler.h include/app_common/app_error.h \
+ include/app_common/app_util.h include/compiler_abstraction.h \
+ include/sd_common/ble_stack_handler_types.h include/s110/ble.h \
+ include/s110/ble_ranges.h include/s110/ble_types.h \
+ include/s110/ble_gap.h include/s110/ble_l2cap.h include/s110/ble_err.h \
+ include/s110/ble_gatt.h include/s110/ble_gattc.h \
+ include/s110/ble_gatts.h include/sd_common/ant_stack_handler_types.h \
+ include/nrf_assert.h include/s110/nrf_soc.h
---
+++ ble/obj/syscall.d
@@ -0,0 +1,25 @@
+obj/syscall.o: sources/syscall.c \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/include/stddef.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/reent.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/_default_types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/lock.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/stdlib.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/alloca.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/errno.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/errno.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/string.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/string.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/stat.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/time.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/time.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/types.h \
+ c:\gnuarm\bin\../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/types.h
